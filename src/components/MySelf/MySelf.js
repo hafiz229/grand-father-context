b8cfb57fbee2a6b0d3f75428d2e4d1436ffe246f
Hafiz <hasnathafiz229>
2021-10-14 04:17:05 +0600

    grand father context family tree

---
+++ src/components/MySelf/MySelf.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MySelf = (props) => {
+  const { house } = props;
+  return (
+    <div>
+      <h2>MySelf</h2>
+      <small>House: {house}</small>
+    </div>
+  );
+};
+
+export default MySelf;
